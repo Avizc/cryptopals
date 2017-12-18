9e08920d69df18a01f7f5295ef51e38d1c3ee82b
Avi <avirzajac@gmail.com>
2017-12-18 18:31:49 -0500

    Added notes, commented out first test string function

--- go/1.go
+++ go/1.go
@@ -1,13 +1,17 @@
 package main
+// Yay! Clean import of packages makes me happy.
+// Packages: https://golang.org/pkg/
 import(
     "fmt"
     // "encoding/hex"
     // "encoding/base64"
 )
-func helloWorld(x string,y string,z string)string{
-    return "I really love "+x+", "+y+", and "+z+"!"
-}
+// This is my first function in Go huzzah!
+// func helloWorld(x string,y string,z string)string{
+//     return "I really love "+x+", "+y+", and "+z+"!"
+// }
+// Print out all the things here.
 func main(){
     fmt.Println("I love rabbits, cheesecake, and cute things!")
-    fmt.Println(helloWorld("rabbits", "cheesecake", "cute things"))
+    // fmt.Println(helloWorld("rabbits", "cheesecake", "cute things"))
 }
