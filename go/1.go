22fd8c905705081699fdc979863857c68777acb2
Avi <avirzajac@gmail.com>
2017-12-19 00:58:11 -0500

    gofmt'd the file, it's so pretty now!

--- go/1.go
+++ go/1.go
@@ -8,36 +8,40 @@
 // Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
 //
 package main
+
 // Yay! Clean import of packages makes me happy.
 // Packages: https://golang.org/pkg/
-import(
+import (
+	"encoding/base64" // https://golang.org/pkg/encoding/base64/
+	"encoding/hex"    // https://golang.org/pkg/encoding/hex/#DecodeString
 	"fmt"
 	"log"
-	"encoding/hex" // https://golang.org/pkg/encoding/hex/#DecodeString
-	"encoding/base64" // https://golang.org/pkg/encoding/base64/
 )
+
 // This is my first function in Go huzzah!
-// func helloWorld(x string,y string,z string)string{
-//     return "I really love "+x+", "+y+", and "+z+"!"
+// func helloWorld(x string, y string, z string) string {
+// 	return "I really love " + x + ", " + y + ", and " + z + "!"
 // }
+
 // Put any string you want here!
-const s="49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-const checkAgainstMe="SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+const s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+const checkAgainstMe = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
 // Print out all the things here.
-func main(){
+func main() {
 	// fmt.Println("I love rabbits, cheesecake, and cute things!")
-	// fmt.Println(helloWorld("rabbits","cheesecake","cute things"))
-	decodedHex,err:=hex.DecodeString(s)
-	if err!=nil{
+	// fmt.Println(helloWorld("rabbits", "cheesecake", "cute things"))
+	decodedHex, err := hex.DecodeString(s)
+	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("This is the decodedHex: %s",decodedHex)
-	encodedBase64:=base64.StdEncoding.EncodeToString([]byte(decodedHex))
-	fmt.Println("\nThis is the encodedBase64: ",encodedBase64)
+	fmt.Printf("This is the decodedHex: %s", decodedHex)
+	encodedBase64 := base64.StdEncoding.EncodeToString([]byte(decodedHex))
+	fmt.Println("\nThis is the encodedBase64: ", encodedBase64)
 	// Oops fix this soon, currently violating the Cryptopals Rule!
-	// if encodedBase64==checkAgainstMe{
+	// if encodedBase64 == checkAgainstMe {
 	// 	fmt.Println("\nSuccess! Your variable encodedBase64 is equal to checkAgainstMe.")
-	// }else{ // Apparently else if/else has to be on the same line as the closing brace? Weird.
+	// } else { // Apparently else if/else has to be on the same line as the closing brace? Weird.
 	// 	fmt.Println("\nUh oh something has gone wrong, try this again?")
 	// }
 }
