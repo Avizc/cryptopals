6714c76cb2ab543fba412bdeeb8532cc1a4acc8d
Avi <avirzajac@gmail.com>
2017-12-19 00:15:08 -0500

    Setting up challenge 2 in Go

---
+++ go/2.go
@@ -0,0 +1,27 @@
+//
+// Challenge 2 - Fixed XOR
+// Write a function that takes two equal-length buffers and produces their XOR combination.
+// If your function works properly, then when you feed it the string:
+// "1c0111001f010100061a024b53535009181c"
+// ...after hex decoding, and when XOR'd against:
+// "686974207468652062756c6c277320657965"
+// ...should produce:
+// "746865206b696420646f6e277420706c6179"
+//
+package main
+import(
+	"fmt"
+	// "log"
+	// "encoding/hex"
+)
+// Test string alice put any string here!
+const alice="1c0111001f010100061a024b53535009181c"
+// Test string whiteRabbit put any string of the same length as alice here!
+const whiteRabbit="686974207468652062756c6c277320657965"
+// Test string wonderland to check if alice and whiteRabbit have been XOR'd properly
+const wonderland="746865206b696420646f6e277420706c6179"
+// Print out all the things here!
+func main(){
+	fmt.Println("This should print alice: ",alice)
+	fmt.Println("This should print whiteRabbit: ",whiteRabbit)
+}
