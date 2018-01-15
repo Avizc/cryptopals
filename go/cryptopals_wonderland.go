8759911444809be18b59702fd8a9a0c6f0383917
Avi <avirzajac@gmail.com>
2018-01-15 02:15:21 -0500

    Added strings for challenge 1, HexToByte currently not working?

--- go/cryptopals_wonderland.go
+++ go/cryptopals_wonderland.go
@@ -5,5 +5,9 @@ import (
 )
 
 func main(){
+	/* Challenge 1 - Convert hex to base64 */
 	alice.Test()
+	const hexCandy string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	const baseCake string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+	// alice.HexToByte(hexCandy)
 }
