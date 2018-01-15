44c80032cd83b23f4629c60d93f265b69c51bdcb
Avi <avirzajac@gmail.com>
2018-01-15 00:48:11 -0500

    Removed first attempt into new separate folder

--- go/rabbit_hole_01/1.go
+++ go/rabbit_hole_01/1.go
@@ -9,7 +9,11 @@
 */
 package rabbit_hole_01
 
-/* Re-doing this challenge, avoid imports as much as possible especially anything from Reflect! ✨ */
+/* 
+	Re-doing this challenge, avoid imports as much as possible especially anything from Reflect! ✨ 
+	Previous first attempt at this challenge can now be found in rabbit_hole_01_old/0.go	
+*/
+
 import (
 	"fmt"
 )
@@ -17,84 +21,3 @@ import (
 func Test(){
 	fmt.Println("I love rabbits, cheesecake, and cute things!")
 }
-
-/*
-	Ignore all the code underneath this message! Prior code before new changes found under commit: ba9ee51577455cad491f788426289c72c0a0b860
-*/
-
-// package main
-
-/* 
-	Yay! Clean import of packages makes me happy.
-	Packages: https://golang.org/pkg/ 
-*/
-// import (
-// 	"encoding/base64" // https://golang.org/pkg/encoding/base64/
-// 	"encoding/hex"    // https://golang.org/pkg/encoding/hex/#DecodeString
-// 	"fmt"
-// 	"log"
-// 	"reflect" // https://golang.org/pkg/reflect/#TypeOf
-// )
-
-/* This is my first function in Go huzzah! */
-// func helloWorld(x string, y string, z string) string {
-// 	return "I really love " + x + ", " + y + ", and " + z + "!"
-// }
-
-/* Put any string you want here! */
-// const s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-// const checkAgainstMe = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-
-/* Print out all the things here. */
-// func main() {
-// 	// fmt.Println("I love rabbits, cheesecake, and cute things!")
-// 	// fmt.Println(helloWorld("rabbits", "cheesecake", "cute things"))
-// 	// decodedHex, err := hex.DecodeString(s)
-// 	// if err != nil {
-// 		// log.Fatal(err)
-// 	// }
-// 	fmt.Printf("This is the decodedHex in symbol: %s", decodedHex)
-// 	// Result: I'm killing your brain like a poisonous mushroom */
-// 	fmt.Println("\nThis is decodedHex in decimal: ", decodedHex)
-// 	// Result: [73 39 109 32 107 105 108 108 105 110 103 32 121 111 117 114 32 98 114 97 105 110 32 108 105 107 101 32 97 32 112 111 105 115 111 110 111 117 115 32 109 117 115 104 114 111 111 109] */
-// 	// fmt.Println("\nThis is the decodedHex typeOf here: ", reflect.TypeOf(decodedHex))
-// 	// Result: []uint8 */
-
-// 	encodedBase64 := base64.StdEncoding.EncodeToString([]byte(decodedHex))
-// 	fmt.Println("\nThis is the encodedBase64 of decodedHex: ", encodedBase64)
-// 	// Result: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
-// 	// fmt.Println("\nThis is the encodedBase64 typeOf here: ", reflect.TypeOf(encodedBase64))
-// 	// Result: string
-
-// 	// I'm not sure if this entire process was necessary, though I'm unsure why it wouldn't convert back cleanly.
-// 	byteEncodedBase64 := []byte(encodedBase64)
-// 	// fmt.Println("\nSee if byteEncodedBase64 works? Here: ", byteEncodedBase64)
-// 	// Result: [83 83 100 116 73 71 116 112 98 71 120 112 98 109 99 103 101 87 57 49 99 105 66 105 99 109 70 112 98 105 66 115 97 87 116 108 73 71 69 103 99 71 57 112 99 50 57 117 98 51 86 122 73 71 49 49 99 50 104 121 98 50 57 116]
-// 	// fmt.Println("\nCheck out the typeOf for byteEncodedBase64: ", reflect.TypeOf(byteEncodedBase64))
-// 	// Result: []uint8
-
-// 	decodedBase64, err := base64.StdEncoding.DecodeString(checkAgainstMe)
-// 	if err != nil {
-// 		fmt.Println("Decode error: ", err)
-// 		return
-// 	}
-// 	fmt.Printf("\nThis is checkAgainstMe base64 decoded in symbol: %s", decodedBase64)
-// 	// Result: I'm killing your brain like a poisonous mushroom
-// 	fmt.Println("\nThis is checkAgainstMe base64 decoded in decimal: ", decodedBase64)
-// 	// Result: [73 39 109 32 107 105 108 108 105 110 103 32 121 111 117 114 32 98 114 97 105 110 32 108 105 107 101 32 97 32 112 111 105 115 111 110 111 117 115 32 109 117 115 104 114 111 111 109]
-// 	// fmt.Println("\nThis is the decodedBase64 typeOf here: ", reflect.TypeOf(decodedBase64))
-// 	// Result: []uint8
-
-// 	// I need to figure out why this is broken!
-// 	// if byteEncodedBase64 == decodedBase64 {
-// 	// 	fmt.Println("\nSuccess! Your variable encodedBase64 is equal to decodedBase64.")
-// 	// } else { // Apparently else if/else has to be on the same line as the closing brace? Weird.
-// 	// 	fmt.Println("\nUh oh something has gone wrong, try this again?")
-// 	// }
-// 	// Currenty result of this conditional is:
-// 	// ./1.go:71: invalid operation: byteEncodedBase64 == decodedBase64 (slice can only be compared to nil)
-
-// 	// Checking if two slices are equal is a package apparently?
-// 	fmt.Println("\nIf this returns 'false' huzzah yay success (until I figure out why byteEncodedBase64 is wrong)!", reflect.DeepEqual(byteEncodedBase64, decodedBase64))
-// 	fmt.Println("\nWait I think I only need to see if decodedHex and decodedBase64 are equal... if this is 'true' whoo success!", reflect.DeepEqual(decodedHex, decodedBase64))
-// }
